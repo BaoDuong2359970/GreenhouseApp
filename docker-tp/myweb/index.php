0dcf00305a25b2c153aed30a499572a1c20bb4e2
Elena Bao Ngoc Duong <150188874+BaoDuong2359970@users.noreply.github.com>
2026-04-27 11:14:24 -0400

    Update index.php

--- docker-tp/myweb/index.php
+++ docker-tp/myweb/index.php
@@ -5,5 +5,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Connected to DB successfully!";
+$result = $conn->query("SELECT * FROM actions");
+
+echo "<h1>Table actions</h1>";
+echo "<table border='1'>";
+echo "<tr><th>ID</th><th>Date</th><th>Commande</th><th>Valeur</th></tr>";
+
+while ($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>".$row["id_action"]."</td>";
+    echo "<td>".$row["id_date"]."</td>";
+    echo "<td>".$row["commande"]."</td>";
+    echo "<td>".$row["valeur"]."</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+
+$conn->close();
 ?>
